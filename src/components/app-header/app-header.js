da522baedc171458d63718d5cdb8b35f7fe5b3a8
Maria Chernova <ma4erme@gmail.com>
2022-12-02 16:04:47 +0400

    made appHeader

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import appHeaderStyles from './app-header.module.css';
-import { Logo, BurgerIcon , Typography } from '@ya.praktikum/react-developer-burger-ui-components'
+import { Logo, BurgerIcon, ListIcon, ProfileIcon, Typography, Box } from '@ya.praktikum/react-developer-burger-ui-components'
 
 export default function AppHeader() {
   return (
     <div className={appHeaderStyles.bg}>
-      <div>
-        
-      </div>
-      <Logo className={appHeaderStyles.logo} alt='Логотип' />
-      <BurgerIcon type="primary" />
+      <nav className={appHeaderStyles.nav}>
+        <ul className={appHeaderStyles.ul}>
+          <li className={`${appHeaderStyles.button} pl-5 pr-5 pb-4 pt-4`}>
+            <BurgerIcon type="primary" />
+            <span className={"text text_type_main-default ml-2"}>Конструктор</span>
+          </li>
+          <li className={`${appHeaderStyles.button} pl-5 pr-5 pb-4 pt-4`}>
+            <ListIcon type="secondary" />
+            <span className={"text text_type_main-default ml-2 text_color_inactive"}>Лента заказов</span>
+          </li>
+        </ul>
+        <Logo className={appHeaderStyles.logo} alt='Логотип' />
+        <ul className={appHeaderStyles.ul}>
+          <li className={`${appHeaderStyles.button} pl-5 pr-5 pb-4 pt-4`}>
+            <ProfileIcon  type="secondary" />
+            <span className={"text text_type_main-default ml-2 text_color_inactive"}>Личный кабинет</span>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
