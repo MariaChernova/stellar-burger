d74e80f5cff49ea0958caba095be044734df77fc
Maria Chernova <ma4erme@gmail.com>
2022-12-06 16:40:42 +0400

    fix BI

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -6,23 +6,32 @@ import Item from '../item/item.js'
 
 
 export default function BurgerIngredients(props) {
-  const {counter, setCount} = React.useState;
-  
-  React.useEffect(() => {
-    setCount = !counter;
-  }, []);
+  // const {counter, setCount} = React.useState;
+
+  // React.useEffect(() => {
+  //   setCount = !counter;
+  // }, []);
+
+  // // const 
+  const [current, setCurrent] = React.useState('one');
 
   return (
-    <div className={`${burgerIngredientsStyles.container} mr-5 mt-5`}>
+    <div className={`${burgerIngredientsStyles.container} mr-5`}>
       <h1 className={'text text_type_main-large mt-5 mb-5'}>Соберите бургер</h1>
-      <div className={burgerIngredientsStyles.tabs}>
-        <tab className={'text text_type_main-default'}>Булки</tab>
-        <tab className={'text text_type_main-default'}>Соусы</tab>
-        <tab className={'text text_type_main-default'}>Начинки</tab>
+      <div className={`${burgerIngredientsStyles.tabs} mb-5`}>
+        <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+          One
+        </Tab>
+        <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+          Two
+        </Tab>
+        <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+          Three
+        </Tab>
       </div>
-      <div className={burgerIngredientsStyles.items}>
-        <h2 className={'text text_type_main-medium mt-5 mb-5'}>Булки</h2>
-        <div className={burgerIngredientsStyles.block}>
+      <div className={`pt-5`}>
+        <h2 className={'text text_type_main-medium mb-5'}>Булки</h2>
+        <div className={`${burgerIngredientsStyles.block} mt-1 ml-4 mr-4 mb-5`}>
           {props.data.filter(obj => obj.type === 'bun').map((obj) => (
             <Item key={obj.id} imageSrc={obj.image} price={obj.price} name={obj.name} />
           ))}
