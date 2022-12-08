0473cf8fb937a7d2f910153c0d28565cadec2610
Maria Chernova <ma4erme@gmail.com>
2022-12-08 21:05:23 +0400

    API

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,15 +4,32 @@ import appStyles from './app.module.css';
 import AppHeader from '../app-header/app-header.js';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients.js';
 import BurgerConstructor from '../burger-constructor/burger-constructor.js';
-import data from '../../utils/data.js';
 
 export default function App() {
+  const apiDomen = 'norma.nomoreparties.space';
+  
+  const [state, setState] = React.useState({ 
+    ingredients: null,
+  })
+
+  React.useEffect(() => {
+    const getIngredients = async () => {
+      const url = `https://${apiDomen}/api/ingredients`;
+      const res = await fetch(url);
+      const data = await res.json();
+      setState({ ingredients: data.data });
+      // TODO: Handle errors
+    }
+
+    getIngredients();
+  }, [])
+
   return (
-    <div>
+    <div className={appStyles.container}>
       <AppHeader  />
       <main className={appStyles.main}>
-        <BurgerIngredients data={data}/>
-        <BurgerConstructor data={data}/>
+        <BurgerIngredients data={state.ingredients}/>
+        <BurgerConstructor data={state.ingredients}/>
       </main>
     </div>
   )
--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -26,15 +26,16 @@ export default function BurgerIngredients(props) {
         <div>
           <h2 className={'text text_type_main-medium mt-2 mb-6'}>Булки</h2>
           <div className={`${burgerIngredientsStyles.block} mt-1 ml-4 mr-4`}>
-            {props.data.filter(obj => obj.type === 'bun').map((obj) => (
-              <Item key={obj.id} imageSrc={obj.image} price={obj.price} name={obj.name} />
-            ))}
+            {props.data !== null && 
+              props.data.filter(obj => obj.type === 'bun').map((obj) => (
+                <Item key={obj.id} imageSrc={obj.image} price={obj.price} name={obj.name} />
+              ))}
           </div>
         </div>
         <div>
           <h2 className={'text text_type_main-medium mt-2 mb-6'}>Соусы</h2>
           <div className={burgerIngredientsStyles.block}>
-            {props.data.filter(obj => obj.type === 'sauce').map((obj) => (
+            {props.data !== null && props.data.filter(obj => obj.type === 'sauce').map((obj) => (
               <Item key={obj.id} imageSrc={obj.image} price={obj.price} name={obj.name} />
             ))}
           </div>
@@ -42,7 +43,7 @@ export default function BurgerIngredients(props) {
         <div>
           <h2 className={'text text_type_main-medium mt-2 mb-6'}>Начинки</h2>
           <div className={burgerIngredientsStyles.block}>
-            {props.data.filter(obj => obj.type === 'main').map((obj) => (
+           {props.data !== null && props.data.filter(obj => obj.type === 'main').map((obj) => (
               <Item key={obj.id} imageSrc={obj.image} price={obj.price} name={obj.name} />
             ))}
           </div>
