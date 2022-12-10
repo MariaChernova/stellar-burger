a1d284d2332c3f3995f8684a70398a83a59f177e
Maria Chernova <ma4erme@gmail.com>
2022-12-10 13:00:20 +0400

    fix modal

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,17 +1,23 @@
 import { Typography, Box, CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import itemStyles from './item.module.css';
+// import Modal from './modal/'
 
 
 export default function Item(props) {
+
+  const handleComponentClick = (item) => {
+    props.openModal();
+  }
+
   return(
-    <div className={`${itemStyles.block} mb-8`}>
-      <Counter count={props.count} size="default" extraClass="m-1" />
-      <img src={props.imageSrc} alt='Image' className={itemStyles.image} />
+    <div className={`${itemStyles.block} mb-8`} onClick={handleComponentClick}>
+      <Counter count={props.data.count} size="default" extraClass="m-1" />
+      <img src={props.data.image} alt='Image' className={itemStyles.image} />
       <div className={`${itemStyles.priceBlock} mt-1 mb-1`}>
-        <p className={'text text_type_digits-default mr-2'}>{props.price}</p>
+        <p className={'text text_type_digits-default mr-2'}>{props.data.price}</p>
         <CurrencyIcon type="primary" />
       </div>
-      <p className={`text text_type_main-default" ${itemStyles.p}`}>{props.name}</p>
+      <p className={`text text_type_main-default" ${itemStyles.p}`}>{props.data.name}</p>
     </div>
   )
 }
