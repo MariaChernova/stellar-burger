dab376eb10dd21a256479be8572392b158639d9c
Maria Chernova <ma4erme@gmail.com>
2022-12-10 13:00:20 +0400

    fix modal

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,12 +4,14 @@ import appStyles from './app.module.css';
 import AppHeader from '../app-header/app-header.js';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients.js';
 import BurgerConstructor from '../burger-constructor/burger-constructor.js';
+import Modal from '../modal/modal.js'
 
 export default function App() {
   const apiDomen = 'norma.nomoreparties.space';
   
   const [state, setState] = React.useState({ 
     ingredients: null,
+    modalIsOpened: false,
   })
 
   React.useEffect(() => {
@@ -24,15 +26,30 @@ export default function App() {
     getIngredients();
   }, [])
 
+  const openModal = () => {
+    setState({
+      ...state,
+      modalIsOpened: true,
+    })
+  }
+
+  const closeModal = () => {
+    setState({
+      ...state,
+      modalIsOpened: false,
+    })
+  }
+
   return (
     <div className={appStyles.container}>
       <AppHeader  />
       <main className={appStyles.main}>
         <h1 className={`${appStyles.constructorTitle} text text_type_main-large mb-5`}>Соберите бургер</h1>
         <div className={appStyles.constructor}>
-          <BurgerIngredients data={state.ingredients}/>
+          <BurgerIngredients data={state.ingredients} openModal={openModal}/>
           <BurgerConstructor data={state.ingredients}/>
         </div>
+        {state.modalIsOpened && <Modal/> }
       </main>
     </div>
   )
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ModalOverlay from './components/modal-overlay/modal-overlay.js';
-import modalStyles from './components/modal/modal.module.css';
+import ReactDOM from 'react-dom';
+import ModalOverlay from '../modal-overlay/modal-overlay.js';
+import modalStyles from './modal.module.css';
 import { Typography, Box, CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
+const modalRoot = document.getElementById('modal-root');
 
 export default function Modal(props) {
-  const modalRoot = ReactDOM.createRoot(
-    document.getElementById('modal-root') as HTMLElement
-  )
-
   return ReactDOM.createPortal(
-    <div className={modalStyles.container}>
-      <div className={modalStyles.bg}>
-        <div className={modalStyles.header}>
-          <p className={modalStyles.title}>{props.title}</p>
-          <CloseIcon type="primary" />
+    (
+      <div className={modalStyles.container}>
+        <ModalOverlay />
+        <div className={`${modalStyles.form} pt-10 pr-10 pb-15 pl-10`}>
+          <div className={modalStyles.header}>
+            <p className={'text text_type_main-large'}>Детали ингредиента</p>
+            <button className={modalStyles.closeButton}><CloseIcon type="primary" /></button>
+          </div>
         </div>
       </div>
-      <ModalOverlay />
-    </div>
-    , modalRoot
-  )
+    ),
+    modalRoot
+  );
 }
