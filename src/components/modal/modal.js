e309a381f34c102f403291151112e963b88df154
Maria Chernova <ma4erme@gmail.com>
2022-12-10 17:05:47 +0400

    refactoring

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import ModalOverlay from '../modal-overlay/modal-overlay.js';
 import modalStyles from './modal.module.css';
 import { Typography, Box, CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import IngredientDetails from '../ingredient-details/ingredient-details.js';
 
 const modalRoot = document.getElementById('modal-root');
 
 export default function Modal(props) {
 
+  const { children, header, onClose } = props;
+
   const close = () => {
-    props.closeModal();
+    onClose();
   }
 
   const escFunction = React.useCallback((event) => {
@@ -27,17 +28,16 @@ export default function Modal(props) {
     };
   }, []);
 
-
   return ReactDOM.createPortal(
     (
       <div className={modalStyles.container}>
         <ModalOverlay onClick={close} />
         <div className={`${modalStyles.form} pt-10 pr-10 pb-15 pl-10`}>
           <div className={modalStyles.header}>
-            <p className={'text text_type_main-large'}>Детали ингредиента</p>
+            <p className={'text text_type_main-large'}>{header}</p>
             <button className={modalStyles.closeButton} onClick={close}><CloseIcon type="primary" /></button>
           </div>
-          <IngredientDetails data={props.data} />
+          {children}
         </div>
       </div>
     ),
