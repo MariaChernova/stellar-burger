f0c662088ee8ddf6529203d75d8dc116630e97bc
Maria Chernova <ma4erme@gmail.com>
2023-01-22 19:58:36 +0400

    scroll

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -4,33 +4,48 @@ import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
 import Item from '../item/item.jsx';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { useInView } from 'react-intersection-observer';
 
+const BUNS_TAB = 'buns';
+const SOUSES_TAB = 'souses';
+const MAIN_TAB = 'main';
 
 export default function BurgerIngredients() {
 
+  const [bunsRef, bunsVisible] = useInView();
+  const [sousesRef, sousesVisible] = useInView();
+  const [mainRef, mainVisible] = useInView();
+  
+
   const ingredients = useSelector(store => store.ingredients.items);
 
-  const [current, setCurrent] = React.useState('one');
+  const [current, setCurrent] = React.useState(BUNS_TAB);
 
   const handleScroll = (evt) => {
-    console.log('скролится')
+    if (bunsVisible) {
+      setCurrent(BUNS_TAB)
+    } else if (sousesVisible) {
+      setCurrent(SOUSES_TAB)
+    } else {
+      setCurrent(MAIN_TAB)
+    }
   }
 
   return (
     <div className={`${burgerIngredientsStyles.container} mr-5`}>
       <div className={`${burgerIngredientsStyles.tabs} mb-8`}>
-        <Tab value="Булки" active={current === 'Булки'} onClick={setCurrent}>
+        <Tab value={BUNS_TAB} active={current === BUNS_TAB} onClick={setCurrent}>
           Булки
         </Tab>
-        <Tab value="Соусы" active={current === 'Соусы'} onClick={setCurrent}>
+        <Tab value={SOUSES_TAB} active={current === SOUSES_TAB} onClick={setCurrent}>
           Соусы
         </Tab>
-        <Tab value="Начинки" active={current === 'Начинки'} onClick={setCurrent}>
+        <Tab value={MAIN_TAB} active={current === MAIN_TAB} onClick={setCurrent}>
           Начинки
         </Tab>
       </div>
       <div className={burgerIngredientsStyles.ingredients} onScroll={handleScroll}>
-        <div>
+        <div ref={bunsRef}>
           <h2 className={'text text_type_main-medium mt-2 mb-6'}>Булки</h2>
           <div className={`${burgerIngredientsStyles.block} mt-1 ml-4 mr-4`}>
             {ingredients !== null && 
@@ -39,7 +54,7 @@ export default function BurgerIngredients() {
               ))}
           </div>
         </div>
-        <div>
+        <div ref={sousesRef}>
           <h2 className={'text text_type_main-medium mt-2 mb-6'}>Соусы</h2>
           <div className={burgerIngredientsStyles.block}>
             {ingredients !== null && ingredients.filter(obj => obj.type === 'sauce').map((obj) => (
@@ -47,7 +62,7 @@ export default function BurgerIngredients() {
             ))}
           </div>
         </div>
-        <div>
+        <div ref={mainRef}>
           <h2 className={'text text_type_main-medium mt-2 mb-6'}>Начинки</h2>
           <div className={burgerIngredientsStyles.block}>
            {ingredients !== null && ingredients.filter(obj => obj.type === 'main').map((obj) => (
